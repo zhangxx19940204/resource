457d0e0f4308b3442dd393fa54eb90a04efc2d19
张祥祥 <zhangxx19940204@163.com>
2020-11-14 16:06:02 +0800

    first commit

---
+++ app/Http/Controllers/Oceanengine/FeiyuController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Oceanengine;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FeiyuController extends Controller
+{
+    //接收飞鱼传送过来的数据
+    public function receive_fei_oceanengine (Request $request){
+
+        return '123456';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/receive_fei_oceanengine', [Oceanengine\FeiyuController::class, 'receive_fei_oceanengine']); //接收头条飞鱼crm发送的数据
+
+
