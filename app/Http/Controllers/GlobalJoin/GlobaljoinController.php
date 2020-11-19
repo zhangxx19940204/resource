7518410e16121ad50308fa71d3694fb5810acfa1
张祥祥 <zhangxx19940204@163.com>
2020-11-19 13:53:27 +0800

    update logger

--- app/Http/Controllers/GlobalJoin/GlobaljoinController.php
+++ app/Http/Controllers/GlobalJoin/GlobaljoinController.php
@@ -18,26 +18,26 @@ class GlobaljoinController extends Controller
         date_default_timezone_set('Asia/Shanghai');
 
         //第一步开始去查询相应的账号对应
-        $config_data = DB::table('res_config')->where('account_id', '=', $para['adv_id'])->where('status','=','1')->first();
-        if (empty($config_data)){
-            //没有对应的生效账号
-            logger('无对应有效账号:'.$para['adv_name'].','.$para['adv_id']);
-            return json_encode(['code'=>-1,'message'=>'fail：接收方账号未配置']);
-        }
+//        $config_data = DB::table('res_config')->where('account_id', '=', $para['adv_id'])->where('status','=','1')->first();
+//        if (empty($config_data)){
+//            //没有对应的生效账号
+//            logger('无对应有效账号:'.$para['adv_name'].','.$para['adv_id']);
+//            return json_encode(['code'=>-1,'message'=>'fail：接收方账号未配置']);
+//        }
+//
+//        //查询到飞鱼的账号，进行数据的整理和存储
+//        $ResData = new ResData;
+//        $ResData->user_id = $config_data->user_id;
+//        $ResData->config_id = $config_data->id;
+//        $ResData->created_at = date('Y-m-d H:i:s');
+//        $ResData->updated_at = date('Y-m-d H:i:s');
+//        $ResData->belong = $config_data->belong;
+//        $ResData->type = $config_data->type;
+//        $ResData->data_json = json_encode($para);
+//        $ResData->data_name = $para['name'];
+//        $ResData->data_phone = $para['telphone'];
+//        $ResData->save();
 
-        //查询到飞鱼的账号，进行数据的整理和存储
-        $ResData = new ResData;
-        $ResData->user_id = $config_data->user_id;
-        $ResData->config_id = $config_data->id;
-        $ResData->created_at = date('Y-m-d H:i:s');
-        $ResData->updated_at = date('Y-m-d H:i:s');
-        $ResData->belong = $config_data->belong;
-        $ResData->type = $config_data->type;
-        $ResData->data_json = json_encode($para);
-        $ResData->data_name = $para['name'];
-        $ResData->data_phone = $para['telphone'];
-        $ResData->save();
-
-        return json_encode(['code'=>0,'message'=>'success']);
+        return json_encode(['success'=>'成功','msg'=>'测试','code'=>200,'response_code'=>0]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 
 Route::any('/receive_fei_oceanengine', [Oceanengine\FeiyuController::class, 'receive_fei_oceanengine']); //接收头条飞鱼crm发送的数据
 Route::any('/get_fast_horse_data', [FastHorse\FasthorseController::class, 'get_fast_horse']); //获取快马账号的数据
+Route::any('/get_global_join', [GlobalJoin\GlobaljoinController::class, 'receive_global_join']); //获取快马账号的数据
