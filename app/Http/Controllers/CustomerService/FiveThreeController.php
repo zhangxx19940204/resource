3a0048d65041de1af877e182307f72975832e784
张祥祥 <zhangxx19940204@163.com>
2021-03-22 14:59:44 +0800

    add 53kf receive message api 0.4

--- app/Http/Controllers/CustomerService/FiveThreeController.php
+++ app/Http/Controllers/CustomerService/FiveThreeController.php
@@ -13,6 +13,12 @@ class FiveThreeController extends Controller
     public function receive_53kf_info(Request $request){
 //        logger('receive_53kf_message_info:'.json_encode($request->all()));
         $customer_data = $request->all(); //{"cmd":"talk_info","msg_id":"ba667079-9f31-4522-9569-0775c14a06fc","content":"这里是url_encode的字符串","token":"rH0D52587Wt54mFok"}
+        //先判断是否是百度关键词推送
+        if (!array_key_exists('cmd',$customer_data)){
+            //此条记录没有cmd字段，可能是百度关键词的推送，先logger，不做操作
+            logger('cmd不存在'.json_encode($customer_data));
+            return ['cmd'=>'OK','token'=>$customer_data['token']];
+        }
         //先去判断token是否在系统中
         $customerService_config = DB::table('customerservice_config')->where('status','1')->where('token',$customer_data['token'])->first();
         if (empty($customerService_config)){
@@ -25,6 +31,9 @@ class FiveThreeController extends Controller
             //整体消息数据的推送
             $complete_data = json_decode(urldecode($customer_data['content']),true);
             $this->receive_53kf_message_info($complete_data,$customerService_config);
+        }else if($customer_data['cmd'] == 'lykeyword'){
+            //百度关键词获取机制
+            logger('百度关键词机制'.json_encode($customer_data));
         }
         return ['cmd'=>'OK','token'=>$customer_data['token']];
     }
