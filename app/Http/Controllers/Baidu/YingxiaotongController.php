49a8936835efe61ce188ee05d3bbda9277414b04
张祥祥 <zhangxx19940204@163.com>
2021-06-25 16:11:21 +0800

    update mail model code 20210625 0.1

--- app/Http/Controllers/Baidu/YingxiaotongController.php
+++ app/Http/Controllers/Baidu/YingxiaotongController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Baidu;
 
 use App\Http\Controllers\Controller;
+use App\Models\ResData;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -14,24 +15,26 @@ class YingxiaotongController extends Controller
         $para = $request->all();
         logger('接收百度营销通发送的数据');
         logger($para);
-        return '';
         //开始处理传来的数据
         date_default_timezone_set('Asia/Shanghai');
 
         //第一步开始去查询相应的账号对应
-        if(!array_key_exists('adv_id',$para)){
-            //adv_id 不存在
-            logger('error参数有误:'.json_encode($para));
+        if(!array_key_exists('pageId',$para) || !array_key_exists('ucid',$para)){
+            //账号id和页面id 不存在
+            logger('接收百度营销通发送的数据error参数有误:'.json_encode($para));
             return json_encode(['code'=>-1,'message'=>'fail：参数有误（adv_id）']);
         }
-        $config_data = DB::table('res_config')->where('account_id', '=', $para['adv_id'])->where('status','=','1')->first();
+        $config_data = DB::table('res_config')
+            ->where('account_id', '=', $para['ucid'])
+            ->where('account', '=', $para['pageId'])
+            ->where('status','=','1')
+            ->first();
         if (empty($config_data)){
             //没有对应的生效账号
-            logger('无对应有效账号:'.$para['adv_name'].','.$para['adv_id']);
-            return json_encode(['code'=>-1,'message'=>'fail：接收方账号未配置']);
+            logger('无对应有效账号:'.$para['ucid'].','.$para['pageId']);
+            return json_encode(['code'=>-1,'message'=>'fail：百度营销通接收方账号未配置']);
         }
-
-        //查询到飞鱼的账号，进行数据的整理和存储
+        //查询到百度营销通的账号，进行数据的整理和存储
         $ResData = new ResData;
         $ResData->user_id = $config_data->user_id;
         $ResData->config_id = $config_data->id;
@@ -41,8 +44,15 @@ class YingxiaotongController extends Controller
         $ResData->belong = $config_data->belong;
         $ResData->type = $config_data->type;
         $ResData->data_json = json_encode($para);
-        $ResData->data_name = $para['name'];
-        $ResData->data_phone = $para['telphone'];
+        $name_key = array_search('name', array_column($para['formDetail'], 'type'));
+        if ($name_key){
+            //知道key值了，去取值
+            $ResData->data_name = $para['formDetail'][$name_key]['value'];
+        }else{
+            //未查询到name字段，没有名字
+            $ResData->data_name = '未知';
+        }
+        $ResData->data_phone = trim($para['cluePhoneNumber']);
         $ResData->save();
 
         return json_encode(['code'=>0,'message'=>'success']);
