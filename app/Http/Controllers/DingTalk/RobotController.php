32f1183896b28bd2c0e8cd7d77c07dbe69f494ff
张祥祥 <zhangxx19940204@163.com>
2021-07-03 17:09:07 +0800

    update dingtalk model code 20210703 0.1

--- app/Http/Controllers/DingTalk/RobotController.php
+++ app/Http/Controllers/DingTalk/RobotController.php
@@ -36,7 +36,10 @@ class RobotController extends Controller
         //判断是否有钉钉用户id
         if (array_key_exists('senderStaffId',$request_data)){
             //钉钉用户id存在，可以调用操作
-            return $this->opera_leave_data($request_data['senderStaffId'],trim($request_data['text']['content']));
+            logger('机器人应用已发布');
+            $res_dingRobot = $this->opera_leave_data($request_data['senderStaffId'],trim($request_data['text']['content']));
+            logger($res_dingRobot);
+            return response()->json($res_dingRobot);
         }else{
             //机器人未发布
             logger('机器人应用未发布');
@@ -51,22 +54,89 @@ class RobotController extends Controller
         $dingTalk_ec_relative_data = DB::table('dingtalk_ec_relative')->where('dingtalk_userid','=',$senderStaffId)->first();
         if (empty($dingTalk_ec_relative_data)){
             //绑定关系为空，则未绑定，跳出
-            return '还未绑定EC账号';
+            logger("还未绑定EC账号");
+            return ['msgtype'=>"text","text"=>["content"=>"EC账号未绑定"],"at"=>["atUserIds"=>[$senderStaffId],"isAtAll"=>false]];
         }else{
             //已绑定成功的数据
             if ($content == '请假'){
                 //用户请假，标记为请假状态，添加到排除列表中
                 logger('请假');
+                $content =$this->change_except_list('leave',$dingTalk_ec_relative_data->ec_userid);
+                return ['msgtype'=>"text","text"=>["content"=>$content],"at"=>["atUserIds"=>[$senderStaffId],"isAtAll"=>false]];
+
             }elseif ($content == '上班'){
                 logger('上班');
+                $content = $this->change_except_list('work',$dingTalk_ec_relative_data->ec_userid);
+                return ['msgtype'=>"text","text"=>["content"=>$content],"at"=>["atUserIds"=>[$senderStaffId],"isAtAll"=>false]];
+
             }elseif ($content == '查询'){
                 logger('查询');
+                return ['msgtype'=>"text","text"=>["content"=>"查询联系管理员"],"at"=>["atUserIds"=>[$senderStaffId],"isAtAll"=>false]];
             }else{
                 //未知关键词，跳过
                 logger('未知');
+                return ['msgtype'=>"text","text"=>["content"=>"未知"],"at"=>["atUserIds"=>[$senderStaffId],"isAtAll"=>false]];
+
             }
-            return '已绑定成功的数据，匹配关键词';
+
         }
     }
+
+    public function change_except_list($type,$ec_userid){
+        //这里去添加和修改请假列表
+        $res_distribution_config_list = DB::table('res_distribution_config')->get();
+        //第一步先判断操作类型
+        if ($type == 'leave'){
+            //增加，添加ecuser到排除列表中
+            //判断是否为null
+            $new_except_arr = [];
+            foreach ($res_distribution_config_list as $res_distribution_config){
+                if (!is_null($res_distribution_config->except_list)){
+                    //排除列表不为null，是个数组
+                    $except_arr = json_decode($res_distribution_config->except_list,true);
+                    $except_arr[] = $ec_userid;
+                }else{
+                    //排除列表为null
+                    $except_arr = [$ec_userid];
+                }
+                $new_except_arr[] = ['id'=>$res_distribution_config->id,'new_except_arr'=>$except_arr];
+            }
+            foreach ($new_except_arr as $new_except){
+                DB::table('res_distribution_config')->where('id', '=',$new_except['id'])
+                    ->update(['except_list' => json_encode($new_except['new_except_arr'])]);
+            }
+            return '请假完毕';
+
+        }elseif ($type == 'work'){
+            //从排除列表中移除
+            //判断是否为null
+            $new_except_arr = [];
+            foreach ($res_distribution_config_list as $res_distribution_config){
+                if (!is_null($res_distribution_config->except_list)){
+                    //排除列表不为null，是个数组
+                    $except_arr = json_decode($res_distribution_config->except_list,true);
+                    //查找值，找到并删除
+                    $search_key = array_search($ec_userid,$except_arr);
+                    if($search_key){//有值，直接删除
+                        unset($except_arr[$search_key]);
+                    }
+                }else{
+                    //排除列表为null
+                    $except_arr = [];
+                }
+                $new_except_arr[] = ['id'=>$res_distribution_config->id,'new_except_arr'=>$except_arr];
+            }
+            foreach ($new_except_arr as $new_except){
+                DB::table('res_distribution_config')->where('id', '=',$new_except['id'])
+                    ->update(['except_list' => json_encode($new_except['new_except_arr'])]);
+            }
+            return '上班中';
+        }else{
+            //类型未知
+            return 'change：$type；未知';
+        }
+    }
+
+
 
 }
