8b1b0ba76f8f549e9ad2930108967369df1d4991
张祥祥 <zhangxx19940204@163.com>
2021-10-01 10:21:41 +0800

    update dingding_feedback code 20211001 0.2

--- resources/views/dingTalk/visit.blade.php
+++ resources/views/dingTalk/visit.blade.php
@@ -239,7 +239,6 @@
 
 
 @section('content')
-<link rel="stylesheet" href="https://www.layuicdn.com/layui/css/layui.css" media="all">
 <link href="{{ asset('dingTalk/investment/layui.css') }}" rel="stylesheet"/>
 <script src="https://cdn.bootcdn.net/ajax/libs/layui/2.6.8/layui.min.js"></script>
 <table id="visit" lay-filter="visit"></table>
