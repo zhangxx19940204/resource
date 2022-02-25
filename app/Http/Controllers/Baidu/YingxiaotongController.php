61426493af7fb3c9e100fd1f62185673d9614f11
张祥祥 <zhangxx19940204@163.com>
2022-02-25 14:25:22 +0800

    add yingxiaotong modal 0.1

--- app/Http/Controllers/Baidu/YingxiaotongController.php
+++ app/Http/Controllers/Baidu/YingxiaotongController.php
@@ -24,25 +24,43 @@ class YingxiaotongController extends Controller
             logger('接收百度营销通发送的数据error参数有误:'.json_encode($para));
             return json_encode(['code'=>-1,'message'=>'fail：参数有误（adv_id）']);
         }
-        $config_data = DB::table('res_config')
+        $config_data_arr = DB::table('res_config')
             ->where('account_id', '=', $para['ucid'])
             // ->where('account', '=', $para['pageId'])
             ->where('status','=','1')
-            ->first();
-        if (empty($config_data)){
+            ->get()->toArray();
+        if (empty($config_data_arr)){
             //没有对应的生效账号
-            logger('无对应有效账号:'.$para['ucid'].','.$para['pageId']);
+            logger('无对应有效账号:'.$para['ucid']);
             return json_encode(['code'=>-1,'message'=>'fail：百度营销通接收方账号未配置']);
+        }else{
+            //主账号配置了，循环账号的页面列表
+            $config_data = [];
+            foreach ($config_data_arr as $single_config){
+                if ($single_config->account == $para['pageId']){
+                    //寻找到了本账号下的对应页面
+                    $config_data = $single_config;
+                    break;
+                }else{
+                    //没找到
+                    continue;
+                }
+            }
+        }
+        //主账号已配置，相应页面结果是
+        if (empty($config_data) && count($config_data_arr) == '1'){
+            //为空,页面未配置，并且只有一条记录，则为全局配置
+            $config_data = $config_data_arr[0];
         }
         //查询到百度营销通的账号，进行数据的整理和存储
         $ResData = new ResData;
-        $ResData->user_id = $config_data->user_id;
-        $ResData->config_id = $config_data->id;
+        $ResData->user_id = $config_data['user_id'];
+        $ResData->config_id = $config_data['id'];
         $ResData->created_at = date('Y-m-d H:i:s');
         $ResData->updated_at = date('Y-m-d H:i:s');
-        $ResData->remarks = $config_data->remarks;
-        $ResData->belong = $config_data->belong;
-        $ResData->type = $config_data->type;
+        $ResData->remarks = $config_data['remarks'];
+        $ResData->belong = $config_data['belong'];
+        $ResData->type = $config_data['type'];
         $ResData->data_json = json_encode($para);
 
         if (!array_key_exists('formDetail',$para) || !is_array($para['formDetail'])){
