8657f00bb5ad62d29aa63fad9cf9fedd81bd799d
张祥祥 <zhangxx19940204@163.com>
2022-03-25 16:51:24 +0800

    updated dingtalk updated user info modal 0.9

--- app/Http/Controllers/DingTalk/CallBackController.php
+++ app/Http/Controllers/DingTalk/CallBackController.php
@@ -25,7 +25,7 @@ class CallBackController extends Controller
         var_dump($res);
         $data = json_decode($res);
         var_dump($text);
-        logger('text'.$text.';res'.$res.';data'.$data);
+        logger('text'.json_encode($text).';res：'.json_encode($res).';data：'.json_encode($data));
         return response()->json(["msg_signature"=>"111108bb8e6dbce3c9671d6fdb69d1506xxxx","timeStamp"=>"1783610513","nonce"=>"123456","encrypt"=>"1ojQf0NSvw2WPvW7LijxS8UvISr8pdDP+rXpPbcLGOmIxxxx"]);
 
 
